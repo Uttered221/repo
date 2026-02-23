4b9571109fe13fbb5b7d058e5ab12589e2fb6049
Uttered221 <Zidane.Cruyff.Palmer@gmail.com>
2026-02-23 19:44:48 +0700

    Refactor database.js to manage user XP and coins
    Refactor database management by removing unused functions and adding XP, coin management, and level bonus functionalities.

--- core/database.js
+++ core/database.js
@@ -1,36 +1,33 @@
-const fs = require("fs");
+const { getUser, saveDB } = require("./data");
 
-const DB_FILE = "./database.json";
-
-let db = {
-  users: {},
-  rooms: {},
-  jackpot: 0,
-  season: 1
+const db = {
+  rooms: {}
 };
 
-function loadDB() {
-  if (fs.existsSync(DB_FILE)) {
-    db = JSON.parse(fs.readFileSync(DB_FILE));
+function addXP(id, amount) {
+  const user = getUser(id);
+  user.xp += amount;
+
+  const need = user.level * 200;
+
+  if (user.xp >= need) {
+    user.xp -= need;
+    user.level++;
   }
+
+  saveDB();
 }
 
-function saveDB() {
-  fs.writeFileSync(DB_FILE, JSON.stringify(db, null, 2));
+function addCoin(id, amount) {
+  const user = getUser(id);
+  user.coin += amount;
+  if (user.coin < 0) user.coin = 0;
+  saveDB();
 }
 
-function getUser(id) {
-  if (!db.users[id]) {
-    db.users[id] = {
-      coin: 1000,
-      xp: 0,
-      level: 1,
-      win: 0,
-      lose: 0,
-      achievement: []
-    };
-  }
-  return db.users[id];
+function levelBonus(id) {
+  const user = getUser(id);
+  return 1 + user.level * 0.05;
 }
 
-module.exports = { db, loadDB, saveDB, getUser };
+module.exports = { db, saveDB, addXP, addCoin, levelBonus };
