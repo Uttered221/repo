6c0ec4b942bf18c11aa86ea66e965061ac298bf4
Uttered221 <Zidane.Cruyff.Palmer@gmail.com>
2026-02-24 16:36:34 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,34 +1,39 @@
 console.log("FILE LOADED");
+
 const { Client, GatewayIntentBits } = require("discord.js");
-// ===== ENV =====
+
+// ==== ENV ====
 const TOKEN = process.env.TOKEN;
 const CLIENT_ID = process.env.CLIENT_ID;
 
-// ===== REQUIRE FILES =====
-const { registerCommands } = require("../core/utils/helpers");
-const loadDB = require("../core/database");
-const { brain } = require("../core/brain");
-// ===== CREATE CLIENT =====
+// ==== REQUIRE FILES ====
+const { registerCommands } = require("./core/utils/helpers");
+const { loadDB } = require("./core/database");
+const { brain } = require("./core/brain");
+
+// ==== CREATE CLIENT ====
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [GatewayIntentBits.Guilds],
 });
-// ===== CHECK ENV =====
+
+// ==== CHECK ENV ====
 if (!TOKEN || !CLIENT_ID) {
-    console.error("TOKEN หรือ CLIENT_ID หาย");
-    process.exit(1);
+  console.error("TOKEN หรือ CLIENT_ID หาย");
+  process.exit(1);
 }
 
 client.once("ready", async () => {
-    console.log("บอทออนไลน์แล้ว: " + client.user.tag);
+  console.log("บอทออนไลน์แล้ว: " + client.user.tag);
 
-    try {
-  await loadDB();
-  await registerCommands(client);
-  console.log("โหลดฐานข้อมูลเรียบร้อย");
-} catch (err) {
-  console.error("มี error ตอนเริ่มระบบ:", err);
+  try {
+    await loadDB();
+    await registerCommands(client);
+    console.log("โหลดฐานข้อมูลเรียบร้อย");
+  } catch (err) {
+    console.error("มี error ตอนเริ่มระบบ:", err);
+  }
 });
-// ===== INTERACTION BRAIN =====
+
 client.on("interactionCreate", async (interaction) => {
   try {
     await brain(interaction);
@@ -37,5 +42,4 @@ client.on("interactionCreate", async (interaction) => {
   }
 });
 
-// ==== LOGIN ====
 client.login(TOKEN);
